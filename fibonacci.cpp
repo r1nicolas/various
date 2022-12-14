dc175e03adc30a3641ff9cf7911c802931e22035
r1nicolas <romain.nicolas@hotmail.fr>
2022-12-14 16:33:19 +0100

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,12 +1,14 @@
-int fibonacci(int n) {
-    int f_n = 1;
-    int f_n_1 = 1;
-    int f_n_2 = 1;
+#include<suite>
 
-    for (int i = 3; i <= n; i++) {
-        f_n = f_n_1 + f_n_2;
-        f_n_2 = f_n_1;
-        f_n_1 = f_n;
-    }
-    return f_n;
+int fibonacci(int n) {
+	int f_n = 1;
+	int f_n_1 = 1;
+	int f_n_2 = 1;
+
+	for (int i = 3; i <= n; i++) {
+		f_n = f_n_1 + f_n_2;
+		f_n_2 = f_n_1;
+	f_n_1 = f_n;
+	}
+	return f_n;
 }
