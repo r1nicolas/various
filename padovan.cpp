b354798c1703f12babddfedcda56d7a2cf0be685
r1nicolas <romain.nicolas@hotmail.fr>
2022-12-14 16:34:36 +0100

    Update padovan.cpp

--- padovan.cpp
+++ padovan.cpp
@@ -1,15 +1,17 @@
-int padovan(int n) {
-    int p_n = 1;
-    int p_n_1 = 1;
-    int p_n_2 = 1;
-    int p_n_3 = 1;
-    
-    for (int i = 4; i <= n; i++) {
-        p_n = p_n_2 + p_n_3;
-        p_n_3 = p_n_2;
-        p_n_2 = p_n_1;
-        p_n_1 = p_n;
-    }
+#include<suite>
 
-    return p_n;
+int padovan(int n) {
+	int p_n = 1;
+	int p_n_1 = 1;
+	int p_n_2 = 1;
+	int p_n_3 = 1;
+
+	for (int i = 4; i <= n; i++) {
+		p_n = p_n_2 + p_n_3;
+		p_n_3 = p_n_2;
+		p_n_2 = p_n_1;
+		p_n_1 = p_n;
+	}
+
+	return p_n;
 }
