6161fdbaed46ba635db469085caf354439486b01
r1nicolas <romain.nicolas@hotmail.fr>
2022-12-16 17:13:39 +0100

    Update my_math.hpp

--- my_math.hpp
+++ my_math.hpp
@@ -17,14 +17,17 @@ public:
 	double getCoefficient(int degree) const;
 	int getDegree() const;
 	double evaluate(double x) const;
-	Polynomial add(const Polynomial& other) const
+	Polynomial add(const Polynomial& other) const;
+	Polynomial subtract(const Polynomial& other) const;
 	std::string toString() const;
 
 private:
 	std::vector<double> coefficients;
+	void removeEmptyCoefficients();
 };
 
 std::ostream& operator<<(std::ostream& os, const Polynomial& p);
 Polynomial operator+(const Polynomial& p1, const Polynomial& p2);
+Polynomial operator-(const Polynomial& p1, const Polynomial& p2);
 
 #endif // MY_MATH_H
