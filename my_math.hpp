fc75cbd93cf5515f0f15268f060e6a6ce89d4954
r1nicolas <romain.nicolas@hotmail.fr>
2022-12-16 23:38:20 +0100

    Update my_math.hpp

--- my_math.hpp
+++ my_math.hpp
@@ -19,6 +19,7 @@ public:
 	double evaluate(double x) const;
 	Polynomial add(const Polynomial& other) const;
 	Polynomial subtract(const Polynomial& other) const;
+	Polynomial multiply(double x);
 	std::string toString() const;
 
 private:
